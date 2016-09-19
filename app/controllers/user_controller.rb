feccbdc7a214657a0861bb83be700a02f791eece
Naveed Shah <naveed@tayybah.com>
2016-09-19 11:26:58 -0400

    Got the flash messages to work

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,5 @@
-require 'rack-flash'
-
 class UserController < ApplicationController
-  use Rack::Flash
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   get '/users/signup' do
@@ -10,14 +8,14 @@ class UserController < ApplicationController
 
   post '/users/signup' do
     if params[:name] == "" || params[:email] == "" || params[:password_digest] == ""
-      flash.now[:message] = "Please fill in all categories"
+      flash[:message] = "Please fill in all categories"
       redirect to '/users/signup'
     else
       submitted_email = params[:email]
       if submitted_email.match(VALID_EMAIL_REGEX) != nil
         User.all.each do |user|
           if user.email == submitted_email
-            flash.now[:message] = "It looks like you already have an account"
+            flash[:message] = "It looks like you already have an account"
             redirect to '/users/login'
 
           end
@@ -36,7 +34,7 @@ class UserController < ApplicationController
         @user.save
         redirect '/users/home'
       else
-        flash.now[:message] = "Please enter a valid email address"
+        flash[:message] = "Please enter a valid email address"
         redirect to '/users/signup'
       end
     end
@@ -47,11 +45,11 @@ class UserController < ApplicationController
     erb :'/users/login'
   end
 
-post '/users/login' do
-  if params[:email] == "" || params[:password_digest] == ""
-    flash.now[:message] = "Please fill in all categories"
-    redirect to '/'
-  else
+  post '/users/login' do
+    if params[:email] == "" || params[:password_digest] == ""
+      flash[:message] = "Please fill in all categories"
+      redirect to '/users/login'
+    else
 
     @user = User.find_by(params)
 
@@ -67,7 +65,7 @@ post '/users/login' do
       @current_location.users << @user
       redirect '/users/home'
     else
-      flash.now[:message] = "Looks like you don't have an account. Sign up here"
+      flash[:message] = "Looks like you don't have an account. Sign up here"
       redirect to '/users/signup'
     end
   end
@@ -86,6 +84,7 @@ get '/users/home' do
   @user = User.find(session[:id])
   @user.location_id = @current_location.id
   @current_location_posts = Post.where('location_id = ?',@current_location.id)
+  flash[:message] = "Welcome Back!"
     erb :'/users/home'
   end
 
@@ -95,6 +94,7 @@ get '/users/home' do
     @current_location = Location.find_by(id: @user.location_id)
     @current_location.users.delete(@user)
     session.clear
+    flash[:message] = "Bye!"
     redirect to '/'
   end
 
@@ -108,23 +108,9 @@ get '/users/home' do
     @user.email = params[:email]
     @user.password_digest = params[:password_digest]
     @user.save
-
+    flash[:message] = "Your information has been updated!"
     redirect '/users/home'
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 end
